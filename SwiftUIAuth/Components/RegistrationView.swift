701e415f54cc7b506a5630298d289d3a46f08a4c
Saqif <saqifhaque@gmail.com>
2024-10-02 17:36:35 +0900

    Registration View Component Created

---
+++ SwiftUIAuth/Components/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  SwiftUIAuth
+//
+//  Created by サキフ ホーク on 2024/10/02.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
