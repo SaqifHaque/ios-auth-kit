4e333ec07dc71b9f903a138b714dcd9ed7ff9622
Saqif <saqifhaque@gmail.com>
2024-10-02 11:46:10 +0900

    Initial Commit

---
+++ SwiftUIAuth/SwiftUIAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAuthApp.swift
+//  SwiftUIAuth
+//
+//  Created by サキフ ホーク on 2024/10/02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
