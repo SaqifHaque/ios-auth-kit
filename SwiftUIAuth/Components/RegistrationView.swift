81f498d4b5a175390f4d499d5ebfb536e97207aa
Saqif <saqifhaque@gmail.com>
2024-10-02 17:36:48 +0900

    Working on Registration View Component

--- SwiftUIAuth/Components/RegistrationView.swift
+++ SwiftUIAuth/Components/RegistrationView.swift
@@ -8,8 +8,76 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    @State private var email = ""
+    @State private var fullName = ""
+    @State private var password = ""
+    @State private var confirmPassword = ""
+    @Environment(\.dismiss) var dismiss
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // Logo
+        Image("auth-image")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 100, height: 200)
+            .padding(.vertical, 32)
+            
+        
+        // form fields
+        VStack (spacing: 24) {
+            InputView(text: $email,
+                      title: "Email Address",
+                      placeholder: "name@example.com")
+            
+            InputView(text: $fullName,
+                      title: "Full Name",
+                      placeholder: "Enter Your Name")
+            
+            InputView(text: $password,
+                      title: "Password",
+                      placeholder: "Enter Your Password",
+                      isSecureField: true)
+            
+            InputView(text: $confirmPassword,
+                      title: "Confirm Password",
+                      placeholder: "Confirm Your Password",
+                      isSecureField: true)
+        }
+        .padding(.horizontal)
+        .padding(.top, 12)
+        
+        // Register Button
+        Button {
+            print("Sign User")
+        } label: {
+            HStack {
+                Text("SIGN UP")
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                Image(systemName: "arrow.right")
+                    .foregroundColor(.white)
+
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+        }
+        .background(Color(.systemBlue))
+        .cornerRadius(15)
+        .padding(.horizontal, 12)
+        .padding(.top, 12)
+        
+        Spacer()
+        
+        Button {
+            dismiss()
+        } label: {
+            HStack(spacing: 3) {
+                Text("Already have an account?")
+                Text("Sign in")
+                    .fontWeight(.bold)
+            }
+            .font(.system(size: 14))
+        }
     }
 }
 
