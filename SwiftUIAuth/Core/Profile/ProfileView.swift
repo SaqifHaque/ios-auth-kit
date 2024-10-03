e2b7a63a28459506810f274e6265d0a81d59ecac
Saqif <saqifhaque@gmail.com>
2024-10-03 16:50:20 +0900

    fetching user in profile view logic added

--- SwiftUIAuth/Core/Profile/ProfileView.swift
+++ SwiftUIAuth/Core/Profile/ProfileView.swift
@@ -8,58 +8,62 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @EnvironmentObject var viewModel: AuthViewModel
+    
     var body: some View {
-        List {
-            Section {
-                HStack {
-                    Text("SH")
-                        .font(.title)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                        .frame(width: 72, height: 72)
-                        .background(Color(.systemGray3))
-                        .clipShape(Circle())
-                    
-                    VStack (alignment: .leading, spacing: 4) {
-                        Text("Saqif Haque")
+        if let user = viewModel.currentUser {
+            List {
+                Section {
+                    HStack {
+                        Text(user.initials)
+                            .font(.title)
                             .fontWeight(.semibold)
-                            .padding(.top, 4)
+                            .foregroundColor(.white)
+                            .frame(width: 72, height: 72)
+                            .background(Color(.systemGray3))
+                            .clipShape(Circle())
                         
-                        Text("test@gmail.com")
-                            .font(.footnote)
-                            .accentColor(.gray)
+                        VStack (alignment: .leading, spacing: 4) {
+                            Text(user.fullname)
+                                .fontWeight(.semibold)
+                                .padding(.top, 4)
+                            
+                            Text(user.email)
+                                .font(.footnote)
+                                .accentColor(.gray)
+                        }
                     }
                 }
-            }
-            
-            Section ("General") {
-                HStack {
-                    SettingsRowView(iconName: "gear",
-                                    title: "Version",
-                                    tintColor: Color(.systemGray))
+                
+                Section ("General") {
+                    HStack {
+                        SettingsRowView(iconName: "gear",
+                                        title: "Version",
+                                        tintColor: Color(.systemGray))
+                        
+                        Spacer()
                     
-                    Spacer()
-                
-                    Text("1.0.0")
-                        .font(.subheadline)
-                        .foregroundColor(.gray)
+                        Text("1.0.0")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                    }
+                    
                 }
                 
-            }
-            
-            Section ("Account") {
-                Button {
-                    print("Sign out...")
-                } label: {
-                    SettingsRowView(iconName: "rectangle.portrait.and.arrow.right", title: "Sign Out", tintColor: Color(.systemGray))
+                Section ("Account") {
+                    Button {
+                        viewModel.signOut()
+                    } label: {
+                        SettingsRowView(iconName: "rectangle.portrait.and.arrow.right", title: "Sign Out", tintColor: Color(.systemGray))
+                    }
+                    
+                    Button {
+                        print("Delete Account...")
+                    } label: {
+                        SettingsRowView(iconName: "xmark.rectangle.portrait", title: "Delete Account", tintColor: Color(.systemGray))
+                    }
+                    
                 }
-                
-                Button {
-                    print("Delete Account...")
-                } label: {
-                    SettingsRowView(iconName: "xmark.rectangle.portrait", title: "Delete Account", tintColor: Color(.systemGray))
-                }
-                
             }
         }
     }
@@ -67,4 +71,5 @@ struct ProfileView: View {
 
 #Preview {
     ProfileView()
+        .environmentObject(AuthViewModel())
 }
