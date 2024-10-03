ff722b7e0c74e1741b506bac52a60ac9f7648b21
Saqif <saqifhaque@gmail.com>
2024-10-03 15:18:02 +0900

    AuthViewModel Created

--- SwiftUIAuth/Core/Authentication/ViewModel/AuthViewModel.swift
+++ SwiftUIAuth/Core/Authentication/ViewModel/AuthViewModel.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+
+class AuthViewModel: ObservableObject {
+    @Published var userSession: FirebaseAuth.User?
+    @Published var currentUser: User?
+    
+    init() {
+        
+    }
+    
+    func signIn(withEmail email: String, password: String) async throws {
+        
+    }
+    
+    func createUser(withEmail email: String, password: String, fullname: String) async throws {
+        
+    }
+    
+    func signOut() {
+        
+    }
+    
+    func deleteAccount() {
+        
+    }
+    
+    func fetchUser() async {
+        
+    }
+}
