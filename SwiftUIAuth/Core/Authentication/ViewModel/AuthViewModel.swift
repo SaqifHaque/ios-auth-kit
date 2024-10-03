c9afbbd2e6b6226afb18b25e2af4f0efb0c6614b
Saqif <saqifhaque@gmail.com>
2024-10-03 16:03:59 +0900

    working on fetchUser()

--- SwiftUIAuth/Core/Authentication/ViewModel/AuthViewModel.swift
+++ SwiftUIAuth/Core/Authentication/ViewModel/AuthViewModel.swift
@@ -15,6 +15,10 @@ class AuthViewModel: ObservableObject {
     
     init() {
         self.userSession = Auth.auth().currentUser
+        
+        Task {
+            await fetchUser()
+        }
     }
     
     func signIn(withEmail email: String, password: String) async throws {
@@ -28,6 +32,7 @@ class AuthViewModel: ObservableObject {
             let user = User(id: result.user.uid, fullname: fullname, email:email)
             let encodedUser = try Firestore.Encoder().encode(user)
             try await Firestore.firestore().collection("users").document(user.id).setData(encodedUser)
+            await fetchUser()
         } catch {
             print("Error Occured while creating User \(error.localizedDescription)")
         }
@@ -42,6 +47,9 @@ class AuthViewModel: ObservableObject {
     }
     
     func fetchUser() async {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
         
+        guard let snapshot = try? await Firestore.firestore().collection("users").document(uid).getDocument() else { return }
+        self.currentUser = try? snapshot.data(as: User.self)
     }
 }
